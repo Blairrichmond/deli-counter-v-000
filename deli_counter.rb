30683a32a550b20361bd0067e150235b30a1d679
Brent  Ransom <sadumedusa@gmail.com>
2018-01-12 16:43:29 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,12 +3,12 @@ def line(katz_deli)
     katz_deli.each_with_index { |name, number| line += " #{number + 1}. #{name}" }
     puts(katz_deli.empty? ? 'The line is currently empty.' : line)
  end
- 
+
  def take_a_number(katz_deli, name)
     katz_deli.push(name)
     puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
  end
- 
+
  def now_serving(katz_deli)
     puts (!katz_deli.empty? ? "Currently serving #{katz_deli[0]}." : "There is nobody waiting to be served!")
     katz_deli.shift
